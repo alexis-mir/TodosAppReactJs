5f27c5308b7893527a2c514862ab72d41055de00
alexis-mir <alexis_mir@hotmail.com>
2021-11-01 15:53:37 -0300

    fix useContext TodoSearch

--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -3,21 +3,18 @@ import "../css/TodoSearch.css";
 import { TodoContext } from "../contexts/TodoContext";
 
 function TodoSearch() {
+  const { searchValue, setSearchValue } = React.useContext(TodoContext);
   const onSearchValueChanged = (e) => {
     console.log(e.target.value);
     setSearchValue(e.target.value);
   };
   return (
-    <TodoContext.Consumer>
-      {({ searchValue, setSearchValue }) => (
-        <input
-          className="TodoSearch"
-          placeholder="Buscar tareas..."
-          value={searchValue}
-          onChange={onSearchValueChanged}
-        />
-      )}
-    </TodoContext.Consumer>
+    <input
+      className="TodoSearch"
+      placeholder="Buscar tareas..."
+      value={searchValue}
+      onChange={onSearchValueChanged}
+    />
   );
 }
 
